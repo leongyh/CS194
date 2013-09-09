5e9facbc01449d2b28a80ef9c58c80573b742b50
Hung <Leong>
2013-09-09 02:10:50 -0700

    A1-Q2

---
+++ Assignment1/Bundle/array.cpp
@@ -0,0 +1,32 @@
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <time.h>
+#include "counters.h"
+
+int main(int argc, char *argv[])
+{
+	hwCounter_t c1;
+	c1.init = false;
+	initTicks(c1);
+
+	//timer for sum
+	uint64_t current_time = getTicks(c1);
+
+	//seed rand gen with time
+	srand(time(NULL));
+
+	//Get the number of the user
+	int N = atoi(argv[1]);
+
+	int arr[N];
+
+	for (int i = 0; i < N; i++)
+	{
+		arr[i] = rand() % N;
+	}
+	
+	uint64_t elapsed_time = getTicks(c1) - current_time;
+
+	printf("Time in Ticks to calculate sum of integers from 0 to 10000 (exclusive): %lu\n", elapsed_time);
+}
