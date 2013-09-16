ac255c6037c8e3546c5034c9154934ced07f115a
Yew_Hung_Leong <cs194-af@hive19.CS.Berkeley.EDU>
2013-09-15 22:46:21 -0700

    a1q4 complete

--- Assignment1/Bundle/mmultiply.cpp
+++ Assignment1/Bundle/mmultiply.cpp
@@ -3,7 +3,7 @@
 #include <cstdlib>
 #include <sys/time.h>
 #include <time.h>
-#include "counter.h"
+#include "counters.h"
 
 void opt_simd_sgemm(float *Y, float *A, float *B, int n);
 void opt_scalar1_sgemm(float *Y, float *A, float *B, int n);
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
 	//seed rand gen with time
 	srand(time(NULL));
 
-	for(i=0; i<n*n; ++i){
+	for(int i=0; i<n*n; ++i){
 		A[i] = (float)rand()/(float)RAND_MAX;
 		B[i] = (float)rand()/(float)RAND_MAX;
 	}
@@ -45,14 +45,17 @@ int main(int argc, char *argv[])
 	gettimeofday(&tv, 0);
 	timestart = tv.tv_sec + 1e-6*tv.tv_usec;
 
-	naive_sgemm(Y, A, B, n);
+	opt_scalar0_sgemm(Y, A, B, n);
 
 	uint64_t exe_ins = getInsns(c) - current_ins;
 	uint64_t elapsed_cyc = getTicks(c1) - current_time;
+	gettimeofday(&tv, 0);
 	timetaken = (tv.tv_sec + 1e-6*tv.tv_usec)-timestart;
 
-	printf("Flops: %f\n", exe_ins/timetaken);
-	printf("IPC: %f\n", elapsed_cyc/timetaken);
+	printf("Ins: %lu\n", exe_ins);
+	printf("Cyc: %lu\n", elapsed_cyc);
+	printf("Sec: %f\n", timetaken);
+	printf("flops, ipc: %f, %lu\n", exe_ins/timetaken, exe_ins/elapsed_cyc);
 
 	delete [] A;
 	delete [] B;
