776e99cf4219197557a1e0c0c9aec27a3689efe5
Hung <Leong>
2013-09-22 12:35:05 -0700

    A2Q1 coded

--- Assignment2/cs194_hw2/p1-matmul/omp_matmul_task.cpp
+++ Assignment2/cs194_hw2/p1-matmul/omp_matmul_task.cpp
@@ -8,19 +8,23 @@ void do_mv(double **a, double **b, double **c, int i)
       c[i][j] += a[i][k]*b[k][j];
 }
 
-void omp_task_matmuld(double **a, 
-		      double **b,
-		      double **c,
-		      int nthr)
+void omp_task_matmuld(double **a, double **b, double **c, int nthr)
 {
   /* this call is needed to set the number
    * of threads */
   omp_set_num_threads(nthr);
 
-  for(int i=0;i<1024;i++)
-    {
-      /* CS194: add pragmas to this loop-nest
-       * to enable OpenMP task parallelism */
-      do_mv(a,b,c,i);
-    }
+  #pragma omp parallel
+  {
+    for(int i=0;i<1024;i++)
+        {
+          /* CS194: add pragmas to this loop-nest
+           * to enable OpenMP task parallelism */
+
+          #pragma omp task shared (a,b,c)
+            do_mv(a,b,c,i);
+          
+          }
+  }
+  
 }
--- Assignment2/cs194_hw2/p1-matmul/pthread_matmul.cpp
+++ Assignment2/cs194_hw2/p1-matmul/pthread_matmul.cpp
@@ -17,21 +17,18 @@ void *matmuld_worker(void *arg)
   double **b = t->b;
   double **c = t->c;
   for(int i = t->start; i < t->end; i++)
-    {
-      for(int j = 0; j < 1024; j++)
-	{
-	  for(int k = 0; k < 1024; k++)
-	    {
+  {
+    for(int j = 0; j < 1024; j++)
+	  {
+  	  for(int k = 0; k < 1024; k++)
+  	  {
 	      c[i][j] += a[i][k]*b[k][j];
 	    }
-	}
-    }
+	  }
+  }
 }
 
-void pthread_matmuld(double **a,
-		     double **b,
-		     double **c,
-		     int nthr)
+void pthread_matmuld(double **a, double **b, double **c, int nthr)
 {
   /* CS194: use pthreads to launch 
    * matrix multply worker threads.
@@ -42,12 +39,19 @@ void pthread_matmuld(double **a,
   pthread_t *thr = new pthread_t[nthr];
   worker_t *tInfo = new worker_t[nthr];
 
-  tInfo[0].a = a;
-  tInfo[0].b = b;
-  tInfo[0].c = c;
-  tInfo[0].start = 0;
-  tInfo[0].end = 1024;
-  matmuld_worker((void*)tInfo);
+  for(int i = 0; i < nthr; ++i){
+    tInfo[0].a = a;
+    tInfo[0].b = b;
+    tInfo[0].c = c;
+    tInfo[0].start = 0;
+    tInfo[0].end = 1024;
+  }
+  
+  for(int i = 0; i < nthr; ++i)
+    pthread_create(&tid[i], NULL,  matmuld_worker, &tInfo[i]);
+
+  for (int i = 0; i < nthr; ++i)
+    pthread_join(&tid[i], NULL);
   
   delete [] thr;
   delete [] tInfo;
