2094041a8b8cecdf954c95fa3191b7c5e35ec3af
Hung <Leong>
2013-10-28 21:19:59 -0700

    refactored kernel reassemble

--- Assignment6/hw6/hw6/radixsort.cl
+++ Assignment6/hw6/hw6/radixsort.cl
@@ -1,154 +1,144 @@
 __kernel void update(__global int *in_zeroes,
-	             __global int *in_ones,
-		     __global int *block_zeroes,
-        	     __global int *block_ones,
-		     int n)
+							 __global int *in_ones,
+							 __global int *block_zeroes,
+							 __global int *block_ones,
+							 int n)
 {
-  size_t idx = get_global_id(0);
-  size_t tid = get_local_id(0);
-  size_t dim = get_local_size(0);
-  size_t gid = get_group_id(0);
+	size_t idx = get_global_id(0);
+	size_t tid = get_local_id(0);
+	size_t dim = get_local_size(0);
+	size_t gid = get_group_id(0);
 
-  if(idx < n && gid > 0)
-  {
-    in_zeroes[idx] = in_zeroes[idx] + block_zeroes[gid-1];
-    in_ones[idx] = in_ones[idx] + block_ones[gid - 1];
-  }
+	if(idx < n && gid > 0)
+	{
+		in_zeroes[idx] = in_zeroes[idx] + block_zeroes[gid-1];
+		in_ones[idx] = in_ones[idx] + block_ones[gid - 1];
+	}
 }
 
 __kernel void scan(__global int *in_zeroes,
-  	           __global int *in_ones,
-		   __global int *zeroes,
-   	           __global int *ones, 
-		   __global int *bzeroes,
-   	           __global int *bones,
-		   /* dynamically sized local (private) memory */
-		   __local int *zeroes_buf,
-       		   __local int *ones_buf,
-		   int v,
-		   int k,
-		   int n)
+							 __global int *in_ones,
+							 __global int *zeroes,
+							 __global int *ones,  
+							 __global int *bzeroes,
+							 __global int *bones,
+							 /* dynamically sized local (private) memory */
+							 __local int *zeroes_buf,
+							 __local int *ones_buf,
+							 int v,
+							 int k,
+							 int n)
 {
-  size_t idx = get_global_id(0);
-  size_t tid = get_local_id(0);
-  size_t dim = get_local_size(0);
-  size_t gid = get_group_id(0);
-  int z, o = 0;
+	size_t idx = get_global_id(0);
+	size_t tid = get_local_id(0);
+	size_t dim = get_local_size(0);
+	size_t gid = get_group_id(0);
+	int z, o = 0;
 
-  if(idx<n){
-    z = in_zeroes[idx];
-    o = in_ones[idx];
-    /* CS194: v==-1 used to signify 
-     * a "normal" additive scan
-     * used to update the partial scans */
-    z = (v==-1) ? z : (v==((z>>k)&0x1)); 
-    o = (v==-1) ? o : (v!=((o>>k)&0x1)); 
+	if(idx<n){
+		z = in_zeroes[idx];
+		o = in_ones[idx];
+		/* CS194: v==-1 used to signify 
+		 * a "normal" additive scan
+		 * used to update the partial scans */
+		z = (v==-1) ? z : (v==((z>>k)&0x1)); 
+		o = (v==-1) ? o : (v!=((o>>k)&0x1)); 
 
-    zeroes_buf[tid] = z;
-    ones_buf[tid] = o;
-  }
-  else{
-    zeroes_buf[tid] = 0;
-    ones_buf[tid] = 0;
-  }
-  
-  barrier(CLK_LOCAL_MEM_FENCE);
+		zeroes_buf[tid] = z;
+		ones_buf[tid] = o;
+	}
+	else{
+		zeroes_buf[tid] = 0;
+		ones_buf[tid] = 0;
+	}
+	
+	barrier(CLK_LOCAL_MEM_FENCE);
 
-  //the first sweep up reduction step
-  int offset = 1;
-  for (int d = dim >> 1; d > 0; d >>= 1)
-  {
-    if(tid < d){
-      zeroes_buf[offset * (2 * tid + 2) - 1] += zeroes_buf[offset * (2 * tid + 1) - 1];
-      ones_buf[offset * (2 * tid + 2) - 1] += ones_buf[offset * (2 * tid + 1) - 1];
-    }
-    offset <<= 1;
-    barrier(CLK_LOCAL_MEM_FENCE);
-  }
+	//the first sweep up reduction step
+	int offset = 1;
+	for (int d = dim >> 1; d > 0; d >>= 1)
+	{
+		if(tid < d){
+			zeroes_buf[offset * (2 * tid + 2) - 1] += zeroes_buf[offset * (2 * tid + 1) - 1];
+			ones_buf[offset * (2 * tid + 2) - 1] += ones_buf[offset * (2 * tid + 1) - 1];
+		}
+		offset <<= 1;
+		barrier(CLK_LOCAL_MEM_FENCE);
+	}
 
-  // at the last value in the local buffer to the other array for 
-  //the update kernel so that this value can be added into the rest
-  // of the values in the output array after this index.
-  bzeroes[gid] = zeroes_buf[dim - 1];
-  bones[gid] = ones_buf[dim - 1];
+	// at the last value in the local buffer to the other array for 
+	//the update kernel so that this value can be added into the rest
+	// of the values in the output array after this index.
+	bzeroes[gid] = zeroes_buf[dim - 1];
+	bones[gid] = ones_buf[dim - 1];
 
-  barrier(CLK_LOCAL_MEM_FENCE | CLK_GLOBAL_MEM_FENCE);
+	barrier(CLK_LOCAL_MEM_FENCE | CLK_GLOBAL_MEM_FENCE);
 
-  // assigned last value in the calculated sweep up reduction step to 0
-  zeroes_buf[dim - 1] = 0;
-  ones_buf[dim - 1] = 0;
+	// assigned last value in the calculated sweep up reduction step to 0
+	zeroes_buf[dim - 1] = 0;
+	ones_buf[dim - 1] = 0;
 
-  // the final sweep down step
-  for(int d = 1; d < dim; d <<= 1){
-    offset >>= 1;
-    barrier(CLK_LOCAL_MEM_FENCE);
+	// the final sweep down step
+	for(int d = 1; d < dim; d <<= 1){
+		offset >>= 1;
+		barrier(CLK_LOCAL_MEM_FENCE);
 
-    if(tid < d){
-      int a = offset * (2 * tid + 1) - 1;
-      int b = offset * (2 * tid +  2) - 1;
+		if(tid < d){
+			int a = offset * (2 * tid + 1) - 1;
+			int b = offset * (2 * tid +  2) - 1;
 
-      int temp0 = zeroes_buf[a];
-      int temp1 = ones_buf[a];
+			int temp0 = zeroes_buf[a];
+			int temp1 = ones_buf[a];
 
-      zeroes_buf[a] = zeroes_buf[b];
-      ones_buf[a] = ones_buf[b];
+			zeroes_buf[a] = zeroes_buf[b];
+			ones_buf[a] = ones_buf[b];
 
-      zeroes_buf[b] += temp0;
-      ones_buf[b] += temp1;
-    }
-  }
+			zeroes_buf[b] += temp0;
+			ones_buf[b] += temp1;
+		}
+	}
 
-  barrier(CLK_LOCAL_MEM_FENCE);
+	barrier(CLK_LOCAL_MEM_FENCE);
 
-  // put the values back into the output array from the local buffer with
-  // a one index shift. The last value the from the other array that was 
-  // previously stored.
-  if(idx < n){
-    if(tid == dim - 1){
-      zeroes[idx] = bzeroes[gid];
-      ones[idx] = bones[gid];
-    }else{
-      zeroes[idx] = zeroes_buf[tid + 1];
-      ones[idx] = ones_buf[tid + 1];
-    }
-  }
-
-  /* CS194: Store partial scans */
-  /*
-  if(idx < n)
-    {
-      out[idx] = buf[r+tid];
-    }
-  */
-  /* CS194: one work-item stores the
-   * work group's total partial
-   * "reduction" */
-  /*
-  if(tid==0)
-    {
-      bout[gid] = buf[r+dim-1];
-    }
-  */
+	// put the values back into the output array from the local buffer with
+	// a one index shift. The last value the from the other array that was 
+	// previously stored.
+	if(idx < n){
+		if(tid == dim - 1){
+			zeroes[idx] = bzeroes[gid];
+			ones[idx] = bones[gid];
+		}else{
+			zeroes[idx] = zeroes_buf[tid + 1];
+			ones[idx] = ones_buf[tid + 1];
+		}
+	}
 }
 
-__kernel void reassemble(__global int *in, __global int *out, __global int *zeroes, __global int *ones, __local int *temp_buf,/* __local int *zeros_buf, __local int *ones_buf,*/ int k, int n){
-  size_t idx = get_global_id(0);
-  size_t tid = get_local_id(0);
-  size_t dim = get_local_size(0);
-  size_t gid = get_group_id(0);
+__kernel void reassemble(__global int *in, 
+								__global int *out, 
+								__global int *zeroes, 
+								__global int *ones, 
+								__local int *temp_buf,
+								__local int *zeros_buf,
+								__local int *ones_buf,
+								int k, 
+								int n){
+	size_t idx = get_global_id(0);
+	size_t tid = get_local_id(0);
+	size_t dim = get_local_size(0);
+	size_t gid = get_group_id(0);
 
-  int offset;
-  if (idx < n){
-    temp_buf[tid] = ((in[idx] >> k) & 0x1);
-    //zeroes_buf[tid] = zeroes[idx];
-    //ones_buf[tid] = ones[idx];
-  }
-  
-  if(temp_buf[tid]){
-    offset = zeroes[n - 1] + ones[idx] - 1;
-  }else{
-    offset = zeroes[idx] - 1;
-  }
-  out[offset] = in[idx];
-  barrier(CLK_GLOBAL_MEM_FENCE);
+	int offset;
+	if (idx < n){
+		temp_buf[tid] = in[idx];
+		zeroes_buf[tid] = zeroes[idx];
+		ones_buf[tid] = ones[idx];
+	}
+	
+	if((temp_buf[tid] >> k) & 0x1){
+		offset = zeroes[n - 1] + ones_buf[tid] - 1;
+	}else{
+		offset = zeroes_buf[tid] - 1;
+	}
+	out[offset] = temp[tid];
 }
--- Assignment6/hw6/hw6/radixsort.cpp
+++ Assignment6/hw6/hw6/radixsort.cpp
@@ -9,224 +9,224 @@
 
 int int_compar(const void *x, const void *y)
 {
-  int i_x = *((int*)x);
-  int i_y = *((int*)y);
-  return i_x > i_y;
+	int i_x = *((int*)x);
+	int i_y = *((int*)y);
+	return i_x > i_y;
 }
 
 void rsort_scan(cl_command_queue &queue,
-    cl_context &context,
-    cl_kernel &scan_kern,
-    cl_kernel &update_kern,
-    cl_mem &in_zeroes,
-    cl_mem &in_ones, 
-    cl_mem &zeroes,
-    cl_mem &ones, 
-    int v,
-    int k,
-    int len);
+		cl_context &context,
+		cl_kernel &scan_kern,
+		cl_kernel &update_kern,
+		cl_mem &in_zeroes,
+		cl_mem &in_ones, 
+		cl_mem &zeroes,
+		cl_mem &ones, 
+		int v,
+		int k,
+		int len);
 
 void rsort_reassemble(cl_command_queue &queue,
-        cl_context &context,
-        cl_kernel &reassemble_kern,
-        cl_mem &in, 
-        cl_mem &out,
-	      cl_mem &zeroes,
-        cl_mem &ones,
-        int k,
-        int len);
+				cl_context &context,
+				cl_kernel &reassemble_kern,
+				cl_mem &in, 
+				cl_mem &out,
+				cl_mem &zeroes,
+				cl_mem &ones,
+				int k,
+				int len);
 
 void cpu_rscan(int *in, int *out, int v, int k, int n)
 {
-  int t = (in[0] >> k) & 0x1;
-  out[0] = (t==v);
-  for(int i = 1; i < n; i++)
-    {
-      t = (in[i] >> k) & 0x1;
-      out[i] = out[i-1]+(t==v);
-    }
+	int t = (in[0] >> k) & 0x1;
+	out[0] = (t==v);
+	for(int i = 1; i < n; i++)
+		{
+			t = (in[i] >> k) & 0x1;
+			out[i] = out[i-1]+(t==v);
+		}
 }
 
 
 int main(int argc, char *argv[])
 {
-  std::string kernel_source_str;
-  
-  std::string arraycompact_kernel_file = 
-    std::string("radixsort.cl");
-  
-  std::list<std::string> kernel_names;
-  std::string scan_name_str = std::string("scan");
-  std::string update_name_str = std::string("update");
-  std::string reassemble_name_str = std::string("reassemble");
+	std::string kernel_source_str;
+	
+	std::string arraycompact_kernel_file = 
+		std::string("radixsort.cl");
+	
+	std::list<std::string> kernel_names;
+	std::string scan_name_str = std::string("scan");
+	std::string update_name_str = std::string("update");
+	std::string reassemble_name_str = std::string("reassemble");
 
-  kernel_names.push_back(scan_name_str);
-  kernel_names.push_back(update_name_str);
-  kernel_names.push_back(reassemble_name_str);
+	kernel_names.push_back(scan_name_str);
+	kernel_names.push_back(update_name_str);
+	kernel_names.push_back(reassemble_name_str);
 
-  cl_vars_t cv; 
-  
-  std::map<std::string, cl_kernel> 
-    kernel_map;
+	cl_vars_t cv; 
+	
+	std::map<std::string, cl_kernel> 
+		kernel_map;
 
-  int c;
-  int n = (1<<20);  
-  int *in, *out;
-  int *c_scan;
-  int n_out=-1;
-  bool silent = false;
+	int c;
+	int n = (1<<20);  
+	int *in, *out;
+	int *c_scan;
+	int n_out=-1;
+	bool silent = false;
 
-  while((c = getopt(argc, argv, "n:s:"))!=-1)
-    {
-      switch(c)
+	while((c = getopt(argc, argv, "n:s:"))!=-1)
+		{
+			switch(c)
 	{
 	case 'n':
-	  n = 1 << atoi(optarg);
-	  break;
+		n = 1 << atoi(optarg);
+		break;
 	case 's':
-	  silent = atoi(optarg) == 1;
-	  break;
+		silent = atoi(optarg) == 1;
+		break;
 	}
-    }
+		}
 
-  in = new int[n];
-  out = new int[n];
-  c_scan = new int[n];
+	in = new int[n];
+	out = new int[n];
+	c_scan = new int[n];
  
-  bzero(out, sizeof(int)*n);
-  bzero(c_scan, sizeof(int)*n);
+	bzero(out, sizeof(int)*n);
+	bzero(c_scan, sizeof(int)*n);
 
-  srand(5);
-  for(int i = 0; i < n; i++)
-    {
-      in[i] = rand();
-    }
+	srand(5);
+	for(int i = 0; i < n; i++)
+		{
+			in[i] = rand();
+		}
 
-  readFile(arraycompact_kernel_file,
-	   kernel_source_str);
-  
-  initialize_ocl(cv);
+	readFile(arraycompact_kernel_file,
+		 kernel_source_str);
+	
+	initialize_ocl(cv);
 
-  compile_ocl_program(kernel_map, cv, 
-		      kernel_source_str.c_str(),
-		      kernel_names);
-  
-  cl_mem g_in, g_zeros, g_ones, g_out;
-  cl_mem g_temp;
-  
-  cl_int err = CL_SUCCESS;
-  g_in = clCreateBuffer(cv.context,CL_MEM_READ_WRITE,
-		       sizeof(int)*n,NULL,&err);
-  CHK_ERR(err);  
+	compile_ocl_program(kernel_map, cv, 
+					kernel_source_str.c_str(),
+					kernel_names);
+	
+	cl_mem g_in, g_zeros, g_ones, g_out;
+	cl_mem g_temp;
+	
+	cl_int err = CL_SUCCESS;
+	g_in = clCreateBuffer(cv.context,CL_MEM_READ_WRITE,
+					 sizeof(int)*n,NULL,&err);
+	CHK_ERR(err);  
 
-  g_ones = clCreateBuffer(cv.context,CL_MEM_READ_WRITE,
-			  sizeof(int)*n,NULL,&err);
-  CHK_ERR(err);
+	g_ones = clCreateBuffer(cv.context,CL_MEM_READ_WRITE,
+				sizeof(int)*n,NULL,&err);
+	CHK_ERR(err);
 
-  g_zeros = clCreateBuffer(cv.context,CL_MEM_READ_WRITE,
-			  sizeof(int)*n,NULL,&err);
-  CHK_ERR(err);
-  
-  g_out = clCreateBuffer(cv.context,CL_MEM_READ_WRITE,
+	g_zeros = clCreateBuffer(cv.context,CL_MEM_READ_WRITE,
+				sizeof(int)*n,NULL,&err);
+	CHK_ERR(err);
+	
+	g_out = clCreateBuffer(cv.context,CL_MEM_READ_WRITE,
 			 sizeof(int)*n,NULL,&err);
-  CHK_ERR(err);
-  
-  //copy data from host CPU to GPU
-  err = clEnqueueWriteBuffer(cv.commands, g_in, true, 0, sizeof(int)*n,
-			     in, 0, NULL, NULL);
-  CHK_ERR(err);
+	CHK_ERR(err);
+	
+	//copy data from host CPU to GPU
+	err = clEnqueueWriteBuffer(cv.commands, g_in, true, 0, sizeof(int)*n,
+					 in, 0, NULL, NULL);
+	CHK_ERR(err);
 
-  err = clEnqueueWriteBuffer(cv.commands, g_out, true, 0, sizeof(int)*n,
-			     c_scan, 0, NULL, NULL);
-  CHK_ERR(err);
-  
-  size_t global_work_size[1] = {n};
-  size_t local_work_size[1] = {128};
+	err = clEnqueueWriteBuffer(cv.commands, g_out, true, 0, sizeof(int)*n,
+					 c_scan, 0, NULL, NULL);
+	CHK_ERR(err);
+	
+	size_t global_work_size[1] = {n};
+	size_t local_work_size[1] = {128};
 
-  adjustWorkSize(global_work_size[0], local_work_size[0]);
-  global_work_size[0] = std::max(local_work_size[0], global_work_size[0]);
-  int left_over = 0;
+	adjustWorkSize(global_work_size[0], local_work_size[0]);
+	global_work_size[0] = std::max(local_work_size[0], global_work_size[0]);
+	int left_over = 0;
 
-  double t0 = timestamp();
+	double t0 = timestamp();
 
-  for(int k = 0; k < 32; k++){
-    //scan zeroes and ones
-    rsort_scan(cv.commands,
-      cv.context,
-      kernel_map[scan_name_str],
-      kernel_map[update_name_str],
-      g_in, 
-      g_in,
-      g_zeros, 
-      g_ones,
-      0,
-      k,
-      n);
+	for(int k = 0; k < 32; k++){
+		//scan zeroes and ones
+		rsort_scan(cv.commands,
+			cv.context,
+			kernel_map[scan_name_str],
+			kernel_map[update_name_str],
+			g_in, 
+			g_in,
+			g_zeros, 
+			g_ones,
+			0,
+			k,
+			n);
 
-    rsort_reassemble(cv.commands,
-      cv.context,
-      kernel_map[reassemble_name_str],
-      g_in,
-      g_out,
-      g_zeros, 
-      g_ones,
-      k,
-      n);
+		rsort_reassemble(cv.commands,
+			cv.context,
+			kernel_map[reassemble_name_str],
+			g_in,
+			g_out,
+			g_zeros, 
+			g_ones,
+			k,
+			n);
 
-    g_temp = g_in;
-    g_in = g_out;
-    g_out = g_temp;
-  }
-  t0 = timestamp() - t0;
+		g_temp = g_in;
+		g_in = g_out;
+		g_out = g_temp;
+	}
+	t0 = timestamp() - t0;
 
-  /* Sort array on CPU for comparison */
-  double t1 = timestamp();
-  qsort(in, n, sizeof(int), int_compar);
-  t1 = timestamp() - t1;
+	/* Sort array on CPU for comparison */
+	double t1 = timestamp();
+	qsort(in, n, sizeof(int), int_compar);
+	t1 = timestamp() - t1;
 
-  err = clEnqueueReadBuffer(cv.commands, g_in, true, 0, sizeof(int)*n,
-			    out, 0, NULL, NULL);
-  CHK_ERR(err);
+	err = clEnqueueReadBuffer(cv.commands, g_in, true, 0, sizeof(int)*n,
+					out, 0, NULL, NULL);
+	CHK_ERR(err);
 
  for(int i = 0; i < n; i++)
-    {
-      if(in[i] != out[i])
+		{
+			if(in[i] != out[i])
 	{
-	  if(!silent)
-	    printf("not sorted @ %d: %d vs %d!\n", i, in[i], out[i]);
- 	    
-	    for(int k = 0; k < 1000; k++)
-//	    	printf("index %d | cpu: %d,  gpu %d\n", k, in[k], out[k]);
-	  goto done;
+		if(!silent)
+			printf("not sorted @ %d: %d vs %d!\n", i, in[i], out[i]);
+			
+			for(int k = 0; k < 1000; k++)
+//        printf("index %d | cpu: %d,  gpu %d\n", k, in[k], out[k]);
+		goto done;
 	}
-    }
+		}
  if(!silent)
-   printf("array sorted\n");
+	 printf("array sorted\n");
 
  if(silent)
-   {
-     printf("%d,%g,%g\n",n,t1,t0);
-   }
+	 {
+		 printf("%d,%g,%g\n",n,t1,t0);
+	 }
  else
-   {
-     printf("GPU: array of length %d sorted in %g seconds\n", n, t0);
+	 {
+		 printf("GPU: array of length %d sorted in %g seconds\n", n, t0);
 
-     printf("CPU: array of length %d sorted in %g seconds\n", n, t1);
-   }
+		 printf("CPU: array of length %d sorted in %g seconds\n", n, t1);
+	 }
  done:
 
-  clReleaseMemObject(g_in); 
-  clReleaseMemObject(g_out);
-  clReleaseMemObject(g_ones);
-  clReleaseMemObject(g_zeros);
-  
+	clReleaseMemObject(g_in); 
+	clReleaseMemObject(g_out);
+	clReleaseMemObject(g_ones);
+	clReleaseMemObject(g_zeros);
+	
 
-  uninitialize_ocl(cv);
+	uninitialize_ocl(cv);
 
-  delete [] in;
-  delete [] out;
-  delete [] c_scan;
-  return 0;
+	delete [] in;
+	delete [] out;
+	delete [] c_scan;
+	return 0;
 }
 
 /* CS194: This function performs an
@@ -238,178 +238,178 @@ void rsort_scan(cl_command_queue &queue,
 		cl_kernel &scan_kern,
 		cl_kernel &update_kern,
 		cl_mem &in_zeroes,
-   		cl_mem &in_ones,
+		cl_mem &in_ones,
 		cl_mem &zeroes,
-	   	cl_mem &ones, 
+		cl_mem &ones, 
 		int v,
 		int k,
 		int len)
 {
-  size_t global_work_size[1] = {len};
-  size_t local_work_size[1] = {128};
-  int left_over = 0;
-  cl_int err;
-  
-  adjustWorkSize(global_work_size[0], local_work_size[0]);
-  global_work_size[0] = std::max(local_work_size[0], global_work_size[0]);
+	size_t global_work_size[1] = {len};
+	size_t local_work_size[1] = {128};
+	int left_over = 0;
+	cl_int err;
+	
+	adjustWorkSize(global_work_size[0], local_work_size[0]);
+	global_work_size[0] = std::max(local_work_size[0], global_work_size[0]);
 
-  left_over = global_work_size[0] / local_work_size[0];
-  
-  cl_mem g_bzeroes = clCreateBuffer(context,CL_MEM_READ_WRITE, 
-				  sizeof(int)*left_over,NULL,&err);
-  CHK_ERR(err);
-  cl_mem g_bones = clCreateBuffer(context,CL_MEM_READ_WRITE, 
-          sizeof(int)*left_over,NULL,&err);
-  CHK_ERR(err);
+	left_over = global_work_size[0] / local_work_size[0];
+	
+	cl_mem g_bzeroes = clCreateBuffer(context,CL_MEM_READ_WRITE, 
+					sizeof(int)*left_over,NULL,&err);
+	CHK_ERR(err);
+	cl_mem g_bones = clCreateBuffer(context,CL_MEM_READ_WRITE, 
+					sizeof(int)*left_over,NULL,&err);
+	CHK_ERR(err);
 
-  err = clSetKernelArg(scan_kern, 0, sizeof(cl_mem), &in_zeroes);
-  CHK_ERR(err);
+	err = clSetKernelArg(scan_kern, 0, sizeof(cl_mem), &in_zeroes);
+	CHK_ERR(err);
 
-  err = clSetKernelArg(scan_kern, 1, sizeof(cl_mem), &in_ones);
-  CHK_ERR(err);
+	err = clSetKernelArg(scan_kern, 1, sizeof(cl_mem), &in_ones);
+	CHK_ERR(err);
 
-  err = clSetKernelArg(scan_kern, 2, sizeof(cl_mem), &zeroes);
-  CHK_ERR(err);
+	err = clSetKernelArg(scan_kern, 2, sizeof(cl_mem), &zeroes);
+	CHK_ERR(err);
 
-  err = clSetKernelArg(scan_kern, 3, sizeof(cl_mem), &ones);
-  CHK_ERR(err);
+	err = clSetKernelArg(scan_kern, 3, sizeof(cl_mem), &ones);
+	CHK_ERR(err);
 
-  err = clSetKernelArg(scan_kern, 4, sizeof(cl_mem), &g_bzeroes);
-  CHK_ERR(err);
+	err = clSetKernelArg(scan_kern, 4, sizeof(cl_mem), &g_bzeroes);
+	CHK_ERR(err);
 
-  err = clSetKernelArg(scan_kern, 5, sizeof(cl_mem), &g_bones);
-  CHK_ERR(err);
+	err = clSetKernelArg(scan_kern, 5, sizeof(cl_mem), &g_bones);
+	CHK_ERR(err);
 
-  err = clSetKernelArg(scan_kern, 6, 2*local_work_size[0]*sizeof(cl_int), NULL);
-  CHK_ERR(err);
+	err = clSetKernelArg(scan_kern, 6, 2*local_work_size[0]*sizeof(cl_int), NULL);
+	CHK_ERR(err);
 
-  err = clSetKernelArg(scan_kern, 7, 2*local_work_size[0]*sizeof(cl_int), NULL);
-  CHK_ERR(err);
+	err = clSetKernelArg(scan_kern, 7, 2*local_work_size[0]*sizeof(cl_int), NULL);
+	CHK_ERR(err);
 
-  /* CS194: v will be either 0 or 1 in order to perform
-   * a scan of bits set (or unset) */
-  err = clSetKernelArg(scan_kern, 8, sizeof(int), &v);
-  CHK_ERR(err);
+	/* CS194: v will be either 0 or 1 in order to perform
+	 * a scan of bits set (or unset) */
+	err = clSetKernelArg(scan_kern, 8, sizeof(int), &v);
+	CHK_ERR(err);
 
-  /* CS194: the current bit position (0 to 31) that
-   * we want to operate on */
-  err = clSetKernelArg(scan_kern, 9, sizeof(int), &k);
-  CHK_ERR(err);
+	/* CS194: the current bit position (0 to 31) that
+	 * we want to operate on */
+	err = clSetKernelArg(scan_kern, 9, sizeof(int), &k);
+	CHK_ERR(err);
 
-  err = clSetKernelArg(scan_kern, 10, sizeof(int), &len);
-  CHK_ERR(err);
+	err = clSetKernelArg(scan_kern, 10, sizeof(int), &len);
+	CHK_ERR(err);
 
-  err = clEnqueueNDRangeKernel(queue,
-			       scan_kern,
-			       1,//work_dim,
-			       NULL, //global_work_offset
-			       global_work_size, //global_work_size
-			       local_work_size, //local_work_size
-			       0, //num_events_in_wait_list
-			       NULL, //event_wait_list
-			       NULL //
-			       );
-  CHK_ERR(err);
+	err = clEnqueueNDRangeKernel(queue,
+						 scan_kern,
+						 1,//work_dim,
+						 NULL, //global_work_offset
+						 global_work_size, //global_work_size
+						 local_work_size, //local_work_size
+						 0, //num_events_in_wait_list
+						 NULL, //event_wait_list
+						 NULL //
+						 );
+	CHK_ERR(err);
 
-  if(left_over > 1)
-    {
-      cl_mem g_bbzeroes = clCreateBuffer(context,CL_MEM_READ_WRITE, 
-				      sizeof(int)*left_over,NULL,&err);
+	if(left_over > 1)
+		{
+			cl_mem g_bbzeroes = clCreateBuffer(context,CL_MEM_READ_WRITE, 
+							sizeof(int)*left_over,NULL,&err);
 
-       cl_mem g_bbones = clCreateBuffer(context,CL_MEM_READ_WRITE, 
-              sizeof(int)*left_over,NULL,&err);
+			 cl_mem g_bbones = clCreateBuffer(context,CL_MEM_READ_WRITE, 
+							sizeof(int)*left_over,NULL,&err);
 
-      /* Recursively perform scan if needed */
-      rsort_scan(queue, context, scan_kern, update_kern, g_bzeroes, g_bones,
-		     g_bbzeroes, g_bbones, -1, k, left_over);
+			/* Recursively perform scan if needed */
+			rsort_scan(queue, context, scan_kern, update_kern, g_bzeroes, g_bones,
+				 g_bbzeroes, g_bbones, -1, k, left_over);
 
-      err = clSetKernelArg(update_kern,0,
-			   sizeof(cl_mem), &zeroes);
-      CHK_ERR(err);
-      err = clSetKernelArg(update_kern,1,
-         sizeof(cl_mem), &ones);
-      CHK_ERR(err);
-      
-      err = clSetKernelArg(update_kern,2,
-			   sizeof(cl_mem), &g_bbzeroes);
-      CHK_ERR(err);
+			err = clSetKernelArg(update_kern,0,
+				 sizeof(cl_mem), &zeroes);
+			CHK_ERR(err);
+			err = clSetKernelArg(update_kern,1,
+				 sizeof(cl_mem), &ones);
+			CHK_ERR(err);
+			
+			err = clSetKernelArg(update_kern,2,
+				 sizeof(cl_mem), &g_bbzeroes);
+			CHK_ERR(err);
 
-      err = clSetKernelArg(update_kern,3,
-         sizeof(cl_mem), &g_bbones);
-      CHK_ERR(err);
+			err = clSetKernelArg(update_kern,3,
+				 sizeof(cl_mem), &g_bbones);
+			CHK_ERR(err);
 
-      err = clSetKernelArg(update_kern,4,
-			   sizeof(int), &len);
-      CHK_ERR(err);
-      
-      /* Update partial scans */
-      err = clEnqueueNDRangeKernel(queue,
-				   update_kern,
-				   1,//work_dim,
-				   NULL, //global_work_offset
-				   global_work_size, //global_work_size
-				   local_work_size, //local_work_size
-				   0, //num_events_in_wait_list
-				   NULL, //event_wait_list
-				   NULL //
-				   );
-      CHK_ERR(err);
-      
-      clReleaseMemObject(g_bbzeroes);
-      clReleaseMemObject(g_bbones);
-    }
+			err = clSetKernelArg(update_kern,4,
+				 sizeof(int), &len);
+			CHK_ERR(err);
+			
+			/* Update partial scans */
+			err = clEnqueueNDRangeKernel(queue,
+					 update_kern,
+					 1,//work_dim,
+					 NULL, //global_work_offset
+					 global_work_size, //global_work_size
+					 local_work_size, //local_work_size
+					 0, //num_events_in_wait_list
+					 NULL, //event_wait_list
+					 NULL //
+					 );
+			CHK_ERR(err);
+			
+			clReleaseMemObject(g_bbzeroes);
+			clReleaseMemObject(g_bbones);
+		}
 
-  clReleaseMemObject(g_bzeroes);
-  clReleaseMemObject(g_bones);
+	clReleaseMemObject(g_bzeroes);
+	clReleaseMemObject(g_bones);
 }
 
 void rsort_reassemble(cl_command_queue &queue,
-        cl_context &context,
-        cl_kernel &reassemble_kern,
-        cl_mem &in, 
-        cl_mem &out,
-        cl_mem &zeroes,
-        cl_mem &ones,
-        int k,
-        int len)
+				cl_context &context,
+				cl_kernel &reassemble_kern,
+				cl_mem &in, 
+				cl_mem &out,
+				cl_mem &zeroes,
+				cl_mem &ones,
+				int k,
+				int len)
 {
-  size_t global_work_size[1] = {len};
-  size_t local_work_size[1] = {128};
-  cl_int err;
+	size_t global_work_size[1] = {len};
+	size_t local_work_size[1] = {128};
+	cl_int err;
 
-  adjustWorkSize(global_work_size[0], local_work_size[0]);
-  global_work_size[0] = std::max(local_work_size[0], global_work_size[0]);
+	adjustWorkSize(global_work_size[0], local_work_size[0]);
+	global_work_size[0] = std::max(local_work_size[0], global_work_size[0]);
 
-  err = clSetKernelArg(reassemble_kern, 0, sizeof(cl_mem), &in);
-  CHK_ERR(err);
+	err = clSetKernelArg(reassemble_kern, 0, sizeof(cl_mem), &in);
+	CHK_ERR(err);
 
-  err = clSetKernelArg(reassemble_kern, 1, sizeof(cl_mem), &out);
-  CHK_ERR(err);
+	err = clSetKernelArg(reassemble_kern, 1, sizeof(cl_mem), &out);
+	CHK_ERR(err);
 
-  err = clSetKernelArg(reassemble_kern, 2, sizeof(cl_mem), &zeroes);
-  CHK_ERR(err);
+	err = clSetKernelArg(reassemble_kern, 2, sizeof(cl_mem), &zeroes);
+	CHK_ERR(err);
 
-  err = clSetKernelArg(reassemble_kern, 3, sizeof(cl_mem), &ones);
-  CHK_ERR(err);
+	err = clSetKernelArg(reassemble_kern, 3, sizeof(cl_mem), &ones);
+	CHK_ERR(err);
 
-  err = clSetKernelArg(reassemble_kern, 4, local_work_size[0]*sizeof(cl_int), NULL);
-  CHK_ERR(err);
+	err = clSetKernelArg(reassemble_kern, 4, local_work_size[0]*sizeof(cl_int), NULL);
+	CHK_ERR(err);
 
-  err = clSetKernelArg(reassemble_kern, 5, sizeof(int), &k);
-  CHK_ERR(err);
+	err = clSetKernelArg(reassemble_kern, 5, sizeof(int), &k);
+	CHK_ERR(err);
 
-  err = clSetKernelArg(reassemble_kern, 6, sizeof(int), &len);
-  CHK_ERR(err);
+	err = clSetKernelArg(reassemble_kern, 6, sizeof(int), &len);
+	CHK_ERR(err);
 
-  err = clEnqueueNDRangeKernel(queue,
-             reassemble_kern,
-             1,//work_dim,
-             NULL, //global_work_offset
-             global_work_size, //global_work_size
-             local_work_size, //local_work_size
-             0, //num_events_in_wait_list
-             NULL, //event_wait_list
-             NULL //
-             );
-  CHK_ERR(err);
+	err = clEnqueueNDRangeKernel(queue,
+						 reassemble_kern,
+						 1,//work_dim,
+						 NULL, //global_work_offset
+						 global_work_size, //global_work_size
+						 local_work_size, //local_work_size
+						 0, //num_events_in_wait_list
+						 NULL, //event_wait_list
+						 NULL //
+						 );
+	CHK_ERR(err);
 }
